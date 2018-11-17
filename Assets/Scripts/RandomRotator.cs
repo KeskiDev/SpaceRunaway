537f2124254c2869e24ebab09e50986a91013e3c
PikkuLy <lyle3olsen@outlook.com>
2018-11-17 14:38:28 -0700

    asteriods ahoy!

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+	public float tumble;
+
+	// Use this for initialization
+	void Start () {
+		var rb = GetComponent<Rigidbody>();
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
